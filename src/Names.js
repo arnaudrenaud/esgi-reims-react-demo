ca2c39b31daa35bee4c69f2c97d0fb797946e24b
Arnaud Renaud <arnaud.renaud@gmail.com>
2023-04-14 14:59:13 +0200

    Add name generator

--- src/Names.js
+++ src/Names.js
@@ -1,5 +1,22 @@
+import { useState } from "react";
+
 const Names = () => {
-  return <h1>Générateur de noms</h1>;
+  const [person, setPerson] = useState(null);
+
+  const fetchPerson = async () => {
+    const response = await fetch("https://randomuser.me/api");
+    const result = await response.json();
+    setPerson(result.results[0]);
+  };
+
+  return (
+    <>
+      <h1>Générateur de noms</h1>
+      <button onClick={fetchPerson}>Nouveau nom</button>
+      <br />
+      {person?.name.first} {person?.name.last}
+    </>
+  );
 };
 
 export default Names;
