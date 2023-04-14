8d9e5e456d8aeda05cb6e305d9dffdaec4600da4
Arnaud Renaud <arnaud.renaud@gmail.com>
2023-04-14 13:01:01 +0200

    Add counter and navigation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { Link } from "react-router-dom";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav className="Nav">
+        <Link to="/" className="Nav-link">
+          Compteur
+        </Link>
+        <Link to="/names" className="Nav-link">
+          Générateur de noms
+        </Link>
+      </nav>
+      <div className="App">
+        Compte :{" "}
+        <span className={count > 5 ? "Count-too-high" : ""}>{count}</span>
+        <br />
+        <button onClick={increment}>Incrémenter</button>
+        <button onClick={decrement}>Décrémenter</button>
+      </div>
+    </>
   );
 }
 
